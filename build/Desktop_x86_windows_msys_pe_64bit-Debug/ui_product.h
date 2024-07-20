88455d39c2b51a098e97dd896d96867eced7d623
marcelinoSamer <marcelino.sedhum@aucegypt.edu>
2024-07-20 18:51:58 +0300

    did some edits on the registeration window

---
+++ build/Desktop_x86_windows_msys_pe_64bit-Debug/ui_product.h
@@ -0,0 +1,46 @@
+/********************************************************************************
+** Form generated from reading UI file 'product.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_PRODUCT_H
+#define UI_PRODUCT_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Product
+{
+public:
+
+    void setupUi(QGroupBox *Product)
+    {
+        if (Product->objectName().isEmpty())
+            Product->setObjectName("Product");
+        Product->resize(967, 607);
+
+        retranslateUi(Product);
+
+        QMetaObject::connectSlotsByName(Product);
+    } // setupUi
+
+    void retranslateUi(QGroupBox *Product)
+    {
+        Product->setWindowTitle(QCoreApplication::translate("Product", "GroupBox", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Product: public Ui_Product {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_PRODUCT_H
